8d1692733d9bee3d953bcd54a73f4235f5ac3c32
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-08-26 00:18:40 -0400

    Vite: use vite.config.js as config base

--- code/lib/builder-vite/src/vite-config.ts
+++ code/lib/builder-vite/src/vite-config.ts
@@ -1,14 +1,19 @@
 import * as path from 'path';
 import fs from 'fs';
-import { Plugin } from 'vite';
+import { loadConfigFromFile, mergeConfig } from 'vite';
+import type {
+  ConfigEnv,
+  InlineConfig as ViteInlineConfig,
+  PluginOption,
+  UserConfig as ViteConfig,
+} from 'vite';
 import viteReact from '@vitejs/plugin-react';
-import type { UserConfig } from 'vite';
-import { allowedEnvPrefix as envPrefix } from './envs';
 import { codeGeneratorPlugin } from './code-generator-plugin';
+import { stringifyProcessEnvs } from './envs';
 import { injectExportOrderPlugin } from './inject-export-order-plugin';
 import { mdxPlugin } from './plugins/mdx-plugin';
 import { noFouc } from './plugins/no-fouc';
-import type { ExtendedOptions } from './types';
+import type { ExtendedOptions, EnvsRaw } from './types';
 
 export type PluginConfigType = 'build' | 'development';
 
@@ -22,23 +27,58 @@ export function readPackageJson(): Record<string, any> | false {
   return JSON.parse(jsonContent);
 }
 
+const configEnvServe: ConfigEnv = {
+  mode: 'development',
+  command: 'serve',
+  ssrBuild: false,
+};
+
+const configEnvBuild: ConfigEnv = {
+  mode: 'production',
+  command: 'build',
+  ssrBuild: false,
+};
+
 // Vite config that is common to development and production mode
 export async function commonConfig(
   options: ExtendedOptions,
   _type: PluginConfigType
-): Promise<UserConfig & { configFile: false; root: string }> {
-  return {
+): Promise<ViteInlineConfig> {
+  const { presets } = options;
+  const configEnv = _type === 'development' ? configEnvServe : configEnvBuild;
+
+  const { config: userConfig = {} } = (await loadConfigFromFile(configEnv)) ?? {};
+
+  const sbConfig = {
     configFile: false,
-    root: path.resolve(options.configDir, '..'),
     cacheDir: 'node_modules/.vite-storybook',
-    envPrefix,
-    define: {},
-    plugins: await pluginConfig(options, _type),
+    root: path.resolve(options.configDir, '..'),
+    plugins: await pluginConfig(options),
+    // If an envPrefix is specified in the vite config, add STORYBOOK_ to it,
+    // otherwise, add VITE_ and STORYBOOK_ so that vite doesn't lose its default.
+    envPrefix: userConfig.envPrefix ? 'STORYBOOK_' : ['VITE_', 'STORYBOOK_'],
   };
+
+  const config: ViteConfig = mergeConfig(userConfig, sbConfig);
+
+  // Sanitize environment variables if needed
+  const envsRaw = await presets.apply<Promise<EnvsRaw>>('env');
+  if (Object.keys(envsRaw).length) {
+    // Stringify env variables after getting `envPrefix` from the  config
+    const envs = stringifyProcessEnvs(envsRaw, config.envPrefix);
+    config.define = {
+      ...config.define,
+      ...envs,
+    };
+  }
+
+  return config;
 }
 
-export async function pluginConfig(options: ExtendedOptions, _type: PluginConfigType) {
-  const { framework, presets } = options;
+export async function pluginConfig(options: ExtendedOptions) {
+  const { presets } = options;
+  const framework = await presets.apply('framework', '', options);
+  const frameworkName: string = typeof framework === 'object' ? framework.name : framework;
   const svelteOptions: Record<string, any> = await presets.apply('svelteOptions', {}, options);
 
   const plugins = [
@@ -47,74 +87,14 @@ export async function pluginConfig(options: ExtendedOptions, _type: PluginConfig
     mdxPlugin(options),
     noFouc(),
     injectExportOrderPlugin,
-    // We need the react plugin here to support MDX.
-    viteReact({
-      // Do not treat story files as HMR boundaries, storybook itself needs to handle them.
-      exclude: [/\.stories\.([tj])sx?$/, /node_modules/].concat(
-        framework === 'react' ? [] : [/\.([tj])sx?$/]
-      ),
-    }),
-    {
-      name: 'vite-plugin-storybook-allow',
-      enforce: 'post',
-      config(config) {
-        // if there is no allow list then Vite allows anything in the root directory
-        // if there is an allow list then Vite allows anything in the listed directories
-        // add the .storybook directory only if there's an allow list so that we don't end up
-        // disallowing the root directory unless it's already disallowed
-        if (config?.server?.fs?.allow) {
-          config.server.fs.allow.push('.storybook');
-        }
-      },
-    },
-  ] as Plugin[];
-  if (framework === 'svelte') {
-    try {
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-      const sveltePlugin = require('@sveltejs/vite-plugin-svelte').svelte;
+  ] as PluginOption[];
 
-      // We need to create two separate svelte plugins, one for stories, and one for other svelte files
-      // because stories.svelte files cannot be hot-module-reloaded.
-      // Suggested in: https://github.com/sveltejs/vite-plugin-svelte/issues/321#issuecomment-1113205509
+  // We need the react plugin here to support MDX in non-react projects.
+  if (frameworkName !== '@storybook/react-vite') {
+    plugins.push(viteReact());
+  }
 
-      // First, create an array containing user exclude patterns, to combine with ours.
-
-      let userExclude = [];
-      if (Array.isArray(svelteOptions?.exclude)) {
-        userExclude = svelteOptions?.exclude;
-      } else if (svelteOptions?.exclude) {
-        userExclude = [svelteOptions?.exclude];
-      }
-
-      // These are the svelte stories we need to exclude from HMR
-      const storyPatterns = ['**/*.story.svelte', '**/*.stories.svelte'];
-      // Non-story svelte files
-      // Starting in 1.0.0-next.42, svelte.config.js is included by default.
-      // We disable that, but allow it to be overridden in svelteOptions
-      plugins.push(sveltePlugin({ ...svelteOptions, exclude: [...userExclude, ...storyPatterns] }));
-      // Svelte stories without HMR
-      const storySveltePlugin = sveltePlugin({
-        ...svelteOptions,
-        exclude: userExclude,
-        include: storyPatterns,
-        hot: false,
-      });
-      plugins.push({
-        // Starting in 1.0.0-next.43, the plugin function returns an array of plugins.  We only want the first one here.
-        ...(Array.isArray(storySveltePlugin) ? storySveltePlugin[0] : storySveltePlugin),
-        name: 'vite-plugin-svelte-stories',
-      });
-    } catch (err) {
-      if ((err as NodeJS.ErrnoException).code === 'MODULE_NOT_FOUND') {
-        throw new Error(
-          '@storybook/builder-vite requires @sveltejs/vite-plugin-svelte to be installed' +
-            ' when using @storybook/svelte.' +
-            '  Please install it and start storybook again.'
-        );
-      }
-      throw err;
-    }
-
+  if (frameworkName === 'svelte') {
     // eslint-disable-next-line import/no-extraneous-dependencies, global-require
     const { loadSvelteConfig } = require('@sveltejs/vite-plugin-svelte');
     const config = { ...loadSvelteConfig(), ...svelteOptions };
@@ -135,12 +115,12 @@ export async function pluginConfig(options: ExtendedOptions, _type: PluginConfig
     plugins.push(svelteDocgen(config));
   }
 
-  if (framework === 'preact') {
+  if (frameworkName === 'preact') {
     // eslint-disable-next-line global-require
     plugins.push(require('@preact/preset-vite').default());
   }
 
-  if (framework === 'glimmerx') {
+  if (frameworkName === 'glimmerx') {
     // eslint-disable-next-line global-require, import/extensions
     const plugin = require('vite-plugin-glimmerx/index.cjs');
     plugins.push(plugin.default());
